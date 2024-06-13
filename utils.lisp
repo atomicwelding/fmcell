7e35c0efc8a172d7b3e750fd5adf26be4da85884
atomicwelding <ctfweld@gmail.com>
2024-06-13 21:34:54 +0200

    matrix & square-matrix utils

--- utils.lisp
+++ utils.lisp
@@ -8,3 +8,19 @@
 
 (defun neq (x y)
   (not (eq x y)))
+
+
+
+(defmacro square-matrix (dim &rest elements)
+  `(matrix ,dim ,dim ,@elements))
+
+
+
+(defmacro matrix (nb-rows nb-columns &rest elements)
+  `(mx 'lla:lla-double ,@(loop for r from 0 below nb-rows
+                               collect (loop for c from 0 below nb-columns 
+                                             collect (nth (+ c (* r nb-columns)) elements)))))
+
+
+
+
